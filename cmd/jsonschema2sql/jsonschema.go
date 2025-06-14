47f3ae45dcc50184bf63269bc1b139ff42ae621f
Andrew Hale <andrew@uqahale2.vpn.uq.edu.au>
2025-06-14 13:57:00 +1000

    wip js2sql

--- cmd/jsonschema2sql/jsonschema.go
+++ cmd/jsonschema2sql/jsonschema.go
@@ -1,28 +1,135 @@
 package jsonschema2sql
 
-import "encoding/json"
-
-type JsonValue interface{}
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+)
 
 const (
-	ARRAY   = "array"
-	BOOLEAN = "boolean"
-	NULL    = "null"
-	NUMBER  = "number"
-	INTEGER = "integer"
-	OBJECT  = "object"
-	STRING  = "string"
+	// JSON
+	JSON_ARRAY   string = "array"
+	JSON_BOOLEAN string = "boolean"
+	JSON_NULL    string = "null"
+	JSON_NUMBER  string = "number"
+	JSON_INTEGER string = "integer"
+	JSON_OBJECT  string = "object"
+	JSON_STRING  string = "string"
+	JSON_NOTYPE  string = ""
+
+	// SQL
+	SQL_BOOLEAN string = "bool"
+	SQL_NUMBER  string = "float"
+	SQL_INTEGER string = "int"
+	SQL_STRING  string = "varchar"
 )
 
 type Property struct {
-	Type       JsonValue       `json:"type"`
+	Type       any             `json:"type"`
 	Reference  string          `json:"$ref"`
 	Properties json.RawMessage `json:"properties"`
 	Items      []string        `json:"items"`
 }
 
 type JsonSchema struct {
-	Type       JsonValue           `json:"type"`
+	Id         string              `json:"id"`
+	Type       any                 `json:"type"`
 	Reference  string              `json:"$ref"`
 	Properties map[string]Property `json:"properties"`
+}
+
+type Field struct {
+	Name string
+	Type string
+}
+
+type (
+	SqlSchema map[string]Table
+	Table     map[string]Field
+)
+
+func (s SqlSchema) ToSql() (string, error) {
+	return "", nil
+}
+
+type KeyedProperty struct {
+	Key      string
+	Property Property
+}
+
+type Stack []KeyedProperty
+
+func (s *Stack) Len() int {
+	return len(*s)
+}
+
+func (s *Stack) Pop() KeyedProperty {
+	i := s.Len() - 1
+	x := (*s)[i]
+	*s = (*s)[i : i+1]
+	return x
+}
+
+func (s *Stack) Push(kp KeyedProperty) {
+	*s = append(*s, kp)
+}
+
+func process(data []byte) (SqlSchema, error) {
+	var j JsonSchema
+	err := json.Unmarshal(data, &j)
+	if err != nil {
+		return SqlSchema{}, err
+	}
+
+	var stack Stack
+	for k, v := range j.Properties {
+		stack.Push(KeyedProperty{k, v})
+	}
+
+	for len(stack) > 0 {
+		kv := stack.Pop()
+		k, v := kv.Key, kv.Property
+
+		switch v.Type.(type) {
+		case []string:
+			field := Field{k, JSON_STRING}
+			fmt.Println(field)
+		case string:
+			switch v.Type.(string) {
+			case JSON_OBJECT:
+			case JSON_ARRAY:
+				array, _ := handleArray(v)
+				fmt.Println(array)
+			case JSON_NOTYPE:
+				field, _ := followReference(j.Reference)
+				fmt.Println(field)
+			case JSON_NUMBER:
+				fallthrough
+			case JSON_STRING:
+				fallthrough
+			case JSON_INTEGER:
+				fallthrough
+			case JSON_BOOLEAN:
+				field := Field{k, v.Type.(string)}
+				fmt.Println(field)
+			default:
+				return SqlSchema{}, fmt.Errorf("failed to process jsonschema: type %v not implemented", v.Type)
+			}
+		default:
+			return SqlSchema{}, fmt.Errorf("failed to process jsonschema: type %T not implemented", v.Type)
+		}
+	}
+
+	return SqlSchema{}, errors.New("failed to find any properties")
+}
+
+func followReference(ref string) (Field, error) {
+	fmt.Println(ref)
+	return Field{}, nil
+}
+
+// can return any of array of types, or array of objects
+func handleArray(prop Property) (any, error) {
+	fmt.Println(prop)
+	return "", nil
 }
--- cmd/jsonschema2sql/jsonschema_test.go
+++ cmd/jsonschema2sql/jsonschema_test.go
@@ -2,10 +2,11 @@ package jsonschema2sql
 
 import (
 	"encoding/json"
-	"fmt"
 	"testing"
 )
 
+// upgrade these to create sql definitions
+// create table schema.release_schema ( id varchar primary key, name varchar, age int )
 func TestNewJsonSchema(t *testing.T) {
 	raw := []byte(`{
   "type": "object",
@@ -19,6 +20,152 @@ func TestNewJsonSchema(t *testing.T) {
 	if err != nil {
 		t.Errorf("failed to unmarshal json: %v", err)
 	}
+}
 
-	fmt.Println(j)
+// upgrade these to create sql definitions
+// create table schema.release_schema ( id varchar primary key, name varchar, age_year int )
+func TestNewJsonSchemaComplex(t *testing.T) {
+	raw := []byte(`{
+  "type": "object",
+  "properties": {
+    "name": { "type": "string" },
+    "age": { "type": "object", "properties": { "year": { "type": int } } }
+  }
+}`)
+	var j JsonSchema
+	err := json.Unmarshal(raw, &j)
+	if err != nil {
+		t.Errorf("failed to unmarshal json: %v", err)
+	}
+
+	nested, err := j.Properties["age"].Properties.MarshalJSON()
+	if err != nil {
+		t.Errorf("failed to marshal nested json: %v", err)
+	}
+
+	var value Property
+	err = json.Unmarshal(nested, &value)
+	if err != nil {
+		t.Errorf("failed to unmarshal json: %v", err)
+	}
+}
+
+// upgrade these to create sql definitions
+// create table schema.release_schema ( id varchar primary key, name varchar, age varchar )
+func TestNewJsonSchemaReference(t *testing.T) {
+	raw := []byte(`{
+  "type": "object",
+  "properties": {
+    "name": { "type": "string" },
+    "age": { "$ref": "#/properties/name" } 
+  }
+}`)
+
+	var j JsonSchema
+	err := json.Unmarshal(raw, &j)
+	if err != nil {
+		t.Errorf("failed to unmarshal json: %v", err)
+	}
+}
+
+// upgrade these to create sql definitions
+// create table schema.release_schema ( id varchar primary key, name varchar, address_street varchar, address_city varchar )
+func TestNewJsonSchemaReferenceComplexDenormalised(t *testing.T) {
+	raw := []byte(`{
+  "type": "object",
+  "properties": {
+    "name": { "type": "string" },
+    "home": { "$ref": "#/definitions/address" }
+  },
+  "definitions": {
+    "address": {
+      "type": "object",
+      "properties": {
+        "street": { "type": "string" },
+        "city": { "type": "string" }
+      }
+    }
+  }
+}`)
+
+	var j JsonSchema
+	err := json.Unmarshal(raw, &j)
+	if err != nil {
+		t.Errorf("failed to unmarshal json: %v", err)
+	}
+}
+
+// upgrade these to create sql definitions
+// create table schema.release_schema ( id varchar primary key, name varchar, address_id string )
+// create table schema.release_schema_addresses ( address_id string primary key, address_street string, address_city string )
+func TestNewJsonSchemaReferenceComplexNormalised(t *testing.T) {
+	raw := []byte(`{
+  "type": "object",
+  "properties": {
+    "name": { "type": "string" },
+    "home": { "$ref": "#/definitions/address" }
+  },
+  "definitions": {
+    "address": {
+      "type": "object",
+      "properties": {
+        "street": { "type": "string" },
+        "city": { "type": "string" }
+      }
+    }
+  }
+}`)
+
+	var j JsonSchema
+	err := json.Unmarshal(raw, &j)
+	if err != nil {
+		t.Errorf("failed to unmarshal json: %v", err)
+	}
+}
+
+// upgrade these to create sql definitions
+// create table schema.release_schema ( id varchar primary key, name varchar, address_id string )
+// create table schema.release_schema_addresses ( address_id string primary key, address_street string, address_city string )
+func TestNewJsonSchemaReferenceComplexNormalisedArray(t *testing.T) {
+	raw := []byte(`{
+  "type": "object",
+  "properties": {
+    "name": { "type": "string" },
+    "houses": { "type": "array", "properties": { "address": { "$ref": "#/definitions/address" } } }
+  },
+  "definitions": {
+    "address": {
+      "type": "object",
+      "properties": {
+        "street": { "type": "string" },
+        "city": { "type": "string" }
+      }
+    }
+  }
+}`)
+
+	var j JsonSchema
+	err := json.Unmarshal(raw, &j)
+	if err != nil {
+		t.Errorf("failed to unmarshal json: %v", err)
+	}
+}
+
+// upgrade these to create sql definitions
+// create table schema.release_schema ( id varchar primary key, name varchar, address_id string )
+// create table schema.release_schema_addresses ( address_id string primary key, address string )
+func TestNewJsonSchemaReferenceArray(t *testing.T) {
+	raw := []byte(`{
+  "type": "object",
+  "properties": {
+    "name": { "type": "string" },
+    "houses": { "type": "array", "properties": { "address": { "type": "string" } } }
+  }
+}`)
+
+	var j JsonSchema
+	err := json.Unmarshal(raw, &j)
+	if err != nil {
+		t.Errorf("failed to unmarshal json: %v", err)
+	}
 }
